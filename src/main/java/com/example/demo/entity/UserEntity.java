14dfadc3b48a5bd56923dc7a8d6934d04e78fd32
bala-amypo <bala@amypo.in>
2025-12-21 09:33:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+
+
+@Entity
+@Table(name = "user",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+    }
+)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.ANALYST;
+}
