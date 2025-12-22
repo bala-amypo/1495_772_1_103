daca8fb04b779fa061b2f1f7ebe5960e396528c5
bala-amypo <bala@amypo.in>
2025-12-22 04:44:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/ScheduleEntity.java
+++ src/main/java/com/example/demo/entity/ScheduleEntity.java
@@ -1,37 +1,17 @@
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-import java.util.List;
-
 @Entity
-public class ShiftTemplate {
+public class GeneratedShiftSchedule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private LocalDate date;
+
+    private Long employeeId;
+
+    private Long shiftTemplateId;
+
     private String shiftName;
 
-    @ElementCollection
-    private List<String> requiredSkills;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getShiftName() {
-        return shiftName;
-    }
-
-    public void setShiftName(String shiftName) {
-        this.shiftName = shiftName;
-    }
-
-    public List<String> getRequiredSkills() {
-        return requiredSkills;
-    }
-
-    public void setRequiredSkills(List<String> requiredSkills) {
-        this.requiredSkills = requiredSkills;
-    }
+    // getters & setters
 }
