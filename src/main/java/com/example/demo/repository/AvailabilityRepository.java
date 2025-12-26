c0c086e24ec97abf7962b4852859fe4a0da75b90
bala-amypo <bala@amypo.in>
2025-12-26 16:50:01 +0000

    User commit

--- src/main/java/com/example/demo/repository/AvailabilityRepository.java
+++ src/main/java/com/example/demo/repository/AvailabilityRepository.java
@@ -1,25 +1,18 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.EmployeeAvailability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface AvailabilityRepository {
+public interface AvailabilityRepository
+        extends JpaRepository<EmployeeAvailability, Long> {
 
-    EmployeeAvailability save(EmployeeAvailability availability);
+    Optional<EmployeeAvailability>
+    findByEmployee_IdAndAvailableDate(Long empId, LocalDate date);
 
-    Optional<EmployeeAvailability> findById(Long id);
-
-    Optional<EmployeeAvailability> findByEmployee_IdAndAvailableDate(
-            Long employeeId, LocalDate date
-    );
-
-    List<EmployeeAvailability> findByAvailableDateAndAvailable(
-            LocalDate date, boolean available
-    );
-
-    List<EmployeeAvailability> findByEmployee_Id(Long employeeId);
-
-    void delete(EmployeeAvailability availability);
+    List<EmployeeAvailability>
+    findByAvailableDateAndAvailable(LocalDate date, Boolean available);
 }
