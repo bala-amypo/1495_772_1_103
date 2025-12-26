74537ef33ee98878cbd8baf6dba32b313b792af4
bala-amypo <bala@amypo.in>
2025-12-26 16:35:00 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeAvailability.java
+++ src/main/java/com/example/demo/model/EmployeeAvailability.java
@@ -21,13 +21,22 @@ public class EmployeeAvailability {
 
     public EmployeeAvailability() {}
 
-    public EmployeeAvailability(Employee employee,
-                                LocalDate availableDate,
-                                Boolean available) {
+    public EmployeeAvailability(Employee employee, LocalDate availableDate, Boolean available) {
         this.employee = employee;
         this.availableDate = availableDate;
         this.available = available;
     }
 
     // getters & setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Employee getEmployee() { return employee; }
+    public void setEmployee(Employee employee) { this.employee = employee; }
+
+    public LocalDate getAvailableDate() { return availableDate; }
+    public void setAvailableDate(LocalDate availableDate) { this.availableDate = availableDate; }
+
+    public Boolean getAvailable() { return available; }
+    public void setAvailable(Boolean available) { this.available = available; }
 }
